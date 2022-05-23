11e18a28f3c8afe062eb041b4ce637d89f71aa1c
olivermatheus97 <math.poliveira97@gmail.com>
2022-05-22 23:13:52 -0300

    home page e css

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./home.css";
+
+function Home() {
+    return(
+        <header class="cabecalho">
+            <a href="index.html"><h1 class="logo">Syonet</h1></a>
+        </header>
+    );
+}
+
+export default Home
